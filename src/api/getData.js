d608ca1f494d28c86e2cb1b726ab007d6524e410
NaveeenkumarSimpleDev <128913146+NaveeenkumarSimpleDev@users.noreply.github.com>
2023-03-26 00:27:14 +0530

    Update getData.js

--- src/api/getData.js
+++ src/api/getData.js
@@ -3,7 +3,10 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const getData = async () => {
 try{
+  console.log('1')
   const response = await axios.get(API_URL);
+  console.log('2')
+ 
   return response.data;
 }catch(err){
   console.log(err)
