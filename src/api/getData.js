19eade4d5ea7b81614ba982513177256d20b91e8
mrNk01 <nn5412042@gmail.com>
2023-03-26 00:21:21 +0530

    port changes

--- src/api/getData.js
+++ src/api/getData.js
@@ -3,7 +3,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const getData = async () => {
 try{
-  const response = await axios.get(API_URL);
+  const response = await axios.get(API_URL).then(res=>res.json());
   return response.data;
 }catch(err){
   console.log(err)
