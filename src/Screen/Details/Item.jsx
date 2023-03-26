66af70cc22625498269b6a57b6bc68468d5a10f5
mrNk01 <nn5412042@gmail.com>
2023-03-26 12:33:43 +0530

    responsive changes

--- src/Screen/Details/Item.jsx
+++ src/Screen/Details/Item.jsx
@@ -12,6 +12,7 @@ function Item(props) {
     localStorage.setItem("CURR_ITEM", JSON.stringify(props));
     ctx.detilsItem = props;
   };
+  const appName = props.appName.length > 12 ? `${props.appName.slice(0,12)}...` : props.appName
   return (
     <>
       <div>
@@ -19,7 +20,9 @@ function Item(props) {
           {props.hotDeal && <div className="fire">🔥</div>}
           <Category>{props.categoryName}</Category>
           <img src={props.logo} className="logo" />
-          <h1>{props.appName}</h1>
+          <div className="appName">
+          <h1>{appName}</h1>
+          </div>
           <p className="link">by Tana FM</p>
           <Link to="/detail" onClick={showDetailsHandler}>
             <button>
--- src/api/getData.js
+++ src/api/getData.js
@@ -3,9 +3,9 @@ const API_URL = import.meta.env.VITE_API_URL;
 console.log(API_URL)
 const getData = async () => {
 try{
-  console.log('1')
+ 
   const response = await axios.get(API_URL);
-  console.log('2')
+
  
   return response.data;
 }catch(err){
